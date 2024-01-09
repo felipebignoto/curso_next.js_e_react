31367119fc2abab49eac7c298e8dffe8d9bba5a9
Felipe-Bignoto <felipebignoto@gmail.com>
2024-01-09 15:27:13 -0300

    inicando renderizacao

---
+++ renderizacao/pages/estatico1.jsx
@@ -0,0 +1,15 @@
+// import { useEffect, useState } from 'react'
+
+export default function Esgtatico1() {
+  // const [num, setNum] = useState(0)
+
+  // useEffect(() => {
+  //   setNum(Math.random())
+  // }, [])
+
+  return (
+    <div>
+      <h1>Estatico #s01</h1>
+    </div>
+  )
+}
