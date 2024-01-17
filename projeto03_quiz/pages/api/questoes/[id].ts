f073db163900b01c1a17bd8171a56b6711e914d4
Felipe-Bignoto <felipebignoto@gmail.com>
2024-01-17 10:25:06 -0300

    vídeo 130

--- projeto03_quiz/pages/api/questoes/[id].ts
+++ projeto03_quiz/pages/api/questoes/[id].ts
@@ -1,5 +1,15 @@
 import questoes from '../bancoDeQuestoes'
 
 export default function questoesProId(req, res) {
-    res.status(200).json(questoes[0].paraObjeto())
+    const idSelecionado = +req.query.id
+    const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+    if(unicaQuestaoOuNada.length === 1){
+        const questaoSelecionado = unicaQuestaoOuNada[0].embaralharRespostas()
+        const obj = questaoSelecionado.responderCom(0).paraObjeto()
+        res.status(200).json(obj)
+    }
+    else{
+        res.status(204).send()
+    }
 }
