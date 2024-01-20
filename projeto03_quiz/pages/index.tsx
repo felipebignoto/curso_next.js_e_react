b55d2cc6635773d09ced818cf472f80a2d4cd894
Felipe-Bignoto <felipebignoto@gmail.com>
2024-01-20 14:45:58 -0300

    atualizando link

--- projeto03_quiz/pages/index.tsx
+++ projeto03_quiz/pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from '@/model/questao'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-const BASE_URL = 'quiz-fbp.vercel.app/api'
+const BASE_URL = 'https://quiz-fbp.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
